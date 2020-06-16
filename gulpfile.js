39625ece6e693c06afe6e976bbbcd69a9b229451
tushar.1210 <ts26192271@gmail.com>
2020-06-16 16:23:00 +0530

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var del = require('del');
+
+function defaultTask(cb) {
+  // place code for your default task here
+  del([
+        'src/*.js'
+      ]);
+  cb();
+}
+exports.default = defaultTask
+
+
+
+
+
