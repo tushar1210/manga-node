ab50c6623ecec5659c71d3fd44baaba64d83e4bb
tushar.1210 <ts26192271@gmail.com>
2020-06-23 03:36:33 +0530

    add scraper

--- src/Handlers/kissmanga.ts
+++ src/Handlers/kissmanga.ts
@@ -1,7 +1,7 @@
 import * as axios from 'axios'
 import * as Fs from 'fs'  ;
-import * as Path from 'path';
 import * as cheerio from 'cheerio';
+import {mangaList} from '../Interfaces/kissManga-mangaList'
 
 const headers = { 
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
@@ -17,35 +17,65 @@ export async function scrapeKissMangaAll(){
     }).then((data)=>{
         var $ = cheerio.load(data.data);
         var title,link,image,lastChapter,lastChapterLink,lastChapterDate,id;
-        var json={
-            success:true,
-            title:"",
-            link:"",
-            image:"",
-            lastChapter:"",
-            lastChapterLink:"",
-            lastChapterDate:""
+
+        var list:mangaList[]=[] ;
+        var ctr=5;
+        var h=$('.page-listing-item');
+        while(ctr-->0){
+            id=String(h.children('.row').children('.col-12').children('.page-item-detail').children('.item-thumb').attr('data-post-id'));
+            link=h.children('.row').children('.col-12').children('.page-item-detail').
+            children('.item-thumb').children().first().attr('href');
+            title=h.children('.row').children('.col-12').children('.page-item-detail').
+            children('.item-thumb').children().first().attr('title');
+            image=h.children('.row').children('.col-12').children('.page-item-detail').
+            children('.item-thumb').children().children().first().attr('src');
+            lastChapter=h.children('.row').children('.col-12').children('.page-item-detail').children('.item-thumb').next().children().next().next().first().children().first().children().first().text();
+            lastChapter = lastChapter.split('\t')[7].trim() ;
+            lastChapterLink=h.children('.row').children('.col-12').children('.page-item-detail').children('.item-thumb').next().children().next().next().first().children().children().children().attr('href');
+            lastChapterDate=h.children('.row').children('.col-12').children('.page-item-detail').children('.item-thumb').next().children().next().next().first().children().first().next().children().next().text();
+            lastChapterDate = lastChapterDate.split('\t')[9] ;
+
+            var json:mangaList={
+                id:id,
+                title:String(title),
+                link:String(link),
+                thumbnail:String(image),
+                lastChapter:String(lastChapter),
+                lastChapterDate:String(lastChapterDate),
+                lastChapterLink:String(lastChapterLink)
+            }
+            
+            list.push(json)
+            
+            id=String(h.children('.row').children('.col-12').next().children('.page-item-detail').children('.item-thumb').attr('data-post-id'));
+            link=h.children('.row').children('.col-12').next().children('.page-item-detail').children('.item-thumb').children().first().attr('href');
+            title=h.children('.row').children('.col-12').next().children('.page-item-detail').
+            children('.item-thumb').children().first().attr('title');
+            image=h.children('.row').children('.col-12').next().children('.page-item-detail').
+            children('.item-thumb').children().children().first().attr('src');
+            lastChapter=h.children('.row').children('.col-12').next().children('.page-item-detail').children('.item-thumb').next().children().next().next().first().children().first().next().children().first().text();
+            lastChapter = lastChapter.split('\t')[7].trim();
+            lastChapterLink=h.children('.row').children('.col-12').next().children('.page-item-detail').children('.item-thumb').next().children().next().next().first().children().children().children().attr('href');
+            lastChapterDate=h.children('.row').children('.col-12').next().children('.page-item-detail').children('.item-thumb').next().children().next().next().first().children().first().next().children().next().text();
+            lastChapterDate = lastChapterDate.split('\t')[9];
+
+            var json:mangaList={
+                id:id,
+                title:String(title),
+                link:String(link),
+                thumbnail:String(image),
+                lastChapter:String(lastChapter),
+                lastChapterDate:String(lastChapterDate),
+                lastChapterLink:String(lastChapterLink)
+            }
+            list.push(json)
+            
+            h=h.next();
         }
-
-        var h=$('.page-listing-item').next()
-
-        id=h.children('.row').children('.col-12').children('.page-item-detail').children('.item-thumb').attr('data-post-id');
-        link=h.children('.row').children('.col-12').children('.page-item-detail').
-        children('.item-thumb').children().first().attr('href');
-        title=h.children('.row').children('.col-12').children('.page-item-detail').
-        children('.item-thumb').children().first().attr('title');
-        image=h.children('.row').children('.col-12').children('.page-item-detail').
-        children('.item-thumb').children().children().first().attr('src');
-        lastChapter=h.children('.row').children('.col-12').children('.page-item-detail').children('.item-thumb').next().children().next().next().first().children().first().next().children().next().text();
-        lastChapter= lastChapter.split(' ')[1] + ' ' + lastChapter.split(' ')[2]
-        lastChapterLink=h.children('.row').children('.col-12').children('.page-item-detail').children('.item-thumb').next().children().next().next().first().children().children().children().attr('href')
-        lastChapterDate=h.children('.row').children('.col-12').children('.page-item-detail').children('.item-thumb').next().children().next().next().first().children().first().next().children().next().text()
-        lastChapterDate = lastChapterDate.split('\t')[9]
-        console.log(id,link,title,image,lastChapter,lastChapterDate,lastChapterLink)
-
+        Fs.appendFileSync('./build/temp/kissmanga.json',JSON.stringify(list))
     })
     .catch((e)=>{
         console.log(e);
-    })
+    });
 
 }
