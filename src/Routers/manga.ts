6eb557d2336d45f244a762b0a1b27a0aaff84692
tushar.1210 <ts26192271@gmail.com>
2020-08-25 01:30:26 +0530

    update scrapper

--- src/Routers/manga.ts
+++ src/Routers/manga.ts
@@ -1,14 +1,13 @@
 import {Request,Response,Router} from 'express';
 import * as handler from '../Handlers/manga';
 import * as Fs from 'fs'  ;
-import {mangaList,searchResult} from '../Interfaces/manga';
-import * as mangaEdenScrapper from '../Scrapper/mangaeden';
-import * as kissMangaScrapper from '../Scrapper/kissmanga';
+
 const router = Router();
 
 const sources={
     0:"https://mangasee123.com"
 }
+
 
 
 
