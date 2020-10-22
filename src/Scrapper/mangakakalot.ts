72b3ac9608f114a503ecb4f0d83797288a489fc8
tushar.1210 <ts26192271@gmail.com>
2020-10-22 23:21:20 +0530

    change code style

--- src/Scrapper/mangakakalot.ts
+++ src/Scrapper/mangakakalot.ts
@@ -1,7 +1,6 @@
 import * as axios from 'axios'
 import * as cheerio from 'cheerio'
 import * as mainInterface from '../interfaces/responses/main'
-import * as helpers from '../helpers/mangakakalot'
 import * as interfaces from '../interfaces/responses/mangakaklot'
 import * as qs from 'querystring'
 import * as Fs from 'fs'
@@ -36,7 +35,7 @@ class Scraper {
       })
         .then((data: axios.AxiosResponse) => {
           try {
-            res = res.concat(helpers.scrapeHotUpdates(data))
+            res = res.concat(this.scrapeHotUpdates(data))
           }
           catch (e) {
             throw new Error(e)
@@ -60,7 +59,7 @@ class Scraper {
       })
         .then((data: axios.AxiosResponse) => {
           try {
-            res = res.concat(helpers.scrapeLatestUpdates(data))
+            res = res.concat(this.scrapeLatestUpdates(data))
             console.log(url)
           }
           catch (e) {
@@ -178,7 +177,7 @@ class Scraper {
       })
         .then((data: axios.AxiosResponse) => {
           try {
-            res = res.concat(helpers.scrapeLatestUpdates(data))
+            res = res.concat(this.scrapeLatestUpdates(data))
           }
           catch (e) {
             throw new Error(e)
@@ -195,6 +194,58 @@ class Scraper {
       }
     })
   }
+
+  scrapeHotUpdates(data: axios.AxiosResponse): mainInterface.hotUpdates[] {
+    let res: mainInterface.hotUpdates[] = []
+    var $ = cheerio.load(data.data, { xmlMode: true })
+    $('.panel-content-genres').children().each((_: number, elem: cheerio.Element) => {
+      let hotUpdateItem: mainInterface.hotUpdates = {
+        title: $('a', elem).attr('title'),
+        imageURL: $('img', elem).attr('src'),
+        source: 'manganelo.com',
+        sourceSpecificName: $('a', elem).attr('href').split('/').slice(-1)[0],
+        currentChapter: $('div', elem).children('a').first().text(),
+        currentChapterURL: $('div', elem).children('a').attr('href'),
+        additionalInfo: {
+          rating: $('em', elem).text(),
+          views: $('.genres-item-view', elem).html(),
+          date: $('.genres-item-time', elem).html(),
+          author: $('.genres-item-author', elem).html(),
+          description: $('.genres-item-description', elem).text().replace(/<[^>]*>?/gm, ''),
+          mangaURL: $('a', elem).attr('href')
+        }
+      }
+      res.push(hotUpdateItem)
+    })
+    return res
+  }
+
+  scrapeLatestUpdates(data: axios.AxiosResponse): mainInterface.latestUpdates[] {
+    let res: mainInterface.latestUpdates[] = []
+    var $ = cheerio.load(data.data, { xmlMode: true })
+    $('.panel-content-genres').children().each((_: number, elem: cheerio.Element) => {
+      let hotUpdateItem: mainInterface.latestUpdates = {
+        title: $('a', elem).attr('title'),
+        imageURL: $('img', elem).attr('src'),
+        source: 'manganelo.com',
+        sourceSpecificName: $('a', elem).attr('href').split('/').slice(-1)[0],
+        currentChapter: $('div', elem).children('a').first().text(),
+        currentChapterURL: $('div', elem).children('a').attr('href'),
+        additionalInfo: {
+          rating: $('em', elem).text(),
+          views: $('.genres-item-view', elem).html(),
+          date: $('.genres-item-time', elem).html(),
+          author: $('.genres-item-author', elem).html(),
+          description: $('.genres-item-description', elem).text().replace(/<[^>]*>?/gm, '').replace('\n', ''),
+          mangaURL: $('a', elem).attr('href')
+        }
+      }
+      res.push(hotUpdateItem)
+    })
+    return res
+  }
+
+
 }
 
 export { Scraper as scraper }
