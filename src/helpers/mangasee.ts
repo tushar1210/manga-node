09202732e6143a36cf288813734ba9a9531dbc94
tushar.1210 <ts26192271@gmail.com>
2020-11-22 00:30:17 +0530

    closes #120; change mangadata structure

--- src/helpers/mangasee.ts
+++ src/helpers/mangasee.ts
@@ -1,3 +1,5 @@
+import { hotUpReq, latestUpReq, allReq, curChapterReq, chapsReq } from '../interfaces/requests/mangasee'
+
 const parseChapNumber = (chapNumber: string): string => {
   let chapNumberR: string = chapNumber.substring(2, 5)
   if (chapNumberR.substring(0, 1) == "0") {
@@ -19,7 +21,39 @@ const chapToken = (chapNumber: string): string => {
   return token
 }
 
+async function currentChapter(allChapterJSON: curChapterReq[], curChp: curChapterReq): Promise<number> {
+  var index: number = -1
+  allChapterJSON.forEach((element: curChapterReq, chapterIndex: number) => {
+    if (curChp.Chapter == element.Chapter) {
+      index = chapterIndex
+    }
+  })
+
+  return index
+}
+
 const thumbnail = (indexName: string): string => {
   return "https://cover.nep.li/cover/" + indexName + '.jpg'
 }
-export { parseChapNumber, chapToken, thumbnail }
+
+const nextChapter = (sourceSpecificName: string, chapterIndex: number | any, allChapterJSON: curChapterReq[]): string => {
+  var val = `https://mangasee123.com/read-online/${sourceSpecificName}-chapter-${parseChapNumber(allChapterJSON[chapterIndex + 1].Chapter)}`
+  if (chapToken(allChapterJSON[chapterIndex + 1].Chapter) == '') {
+    val += `.html`
+  } else {
+    val += `${chapToken(allChapterJSON[chapterIndex + 1].Chapter)}.html`
+  }
+  return val
+}
+
+const previousChapter = (sourceSpecificName: string, chapterIndex: number | any, allChapterJSON: curChapterReq[]): string => {
+  var val = `https://mangasee123.com/read-online/${sourceSpecificName}-chapter-${parseChapNumber(allChapterJSON[chapterIndex - 1].Chapter)}`
+  if (chapToken(allChapterJSON[chapterIndex - 1].Chapter) == '') {
+    val += `.html`
+  } else {
+    val += `${chapToken(allChapterJSON[chapterIndex - 1].Chapter)}.html`
+  }
+  return val
+}
+
+export { parseChapNumber, chapToken, thumbnail, nextChapter, previousChapter, currentChapter }
